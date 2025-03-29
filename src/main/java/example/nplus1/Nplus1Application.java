39fcda7f433335cd5ef048a6bee018d4ec33c804
hyun <apfhd0257@naver.com>
2025-03-29 18:09:16 +0900

    init

---
+++ src/main/java/example/nplus1/Nplus1Application.java
@@ -0,0 +1,13 @@
+package example.nplus1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nplus1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nplus1Application.class, args);
+    }
+
+}
---
+++ src/test/java/example/nplus1/Nplus1ApplicationTests.java
@@ -0,0 +1,13 @@
+package example.nplus1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nplus1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
